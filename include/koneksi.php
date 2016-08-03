6a9db3b3406fab0c04df8a4c04a83b9087f2783d
Ramdhan Rizki <Ramdhan Rizki>
2016-08-03 12:56:20 +0700

    Add Web Config and Edit Koneksi.php

--- include/koneksi.php
+++ include/koneksi.php
@@ -1,8 +1,14 @@
 <?php 
-$user="root";
-$host="localhost";
-$pass="";
-$db="ppdb";
+// $user="root";
+// $host="localhost";
+// $pass="";
+// $db="ppdb";
+
+$user="b7c9fbc2e250c5";
+$host="ap-cdbr-azure-east-c.cloudapp.net";
+$pass="65b2bf44";
+$db="psb";
+
 mysql_connect("$host","$user","$pass");
 mysql_select_db("$db");
 
